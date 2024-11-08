22cde0b8d40e0d9bbcfa243605e587ab33ccd178
Fahima Alo <fahimaalo223@gmail.com>
2024-11-08 11:05:49 +0600

    phones varChat added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { LineChart } from 'recharts'
 import './App.css'
 import NavBar from './Components/NavBar/NavBar'
 import PriceOptions from './Components/PriceOptions/PriceOptions'
+import Phones from './Components/Phones/Phones'
 // import DaisyNav from './Components/DaisyNav/DaisyNav'
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <h1 className='text-7xl bg-red-600'>Vite + React</h1>
       <PriceOptions></PriceOptions>
       <LineChart></LineChart>
+      <Phones></Phones>
 
 
     
